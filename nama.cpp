3e328876d8514a843bf0d1093ddb5b60d454a1a4
Qadri54 <124790452+Qadri54@users.noreply.github.com>
2023-10-01 21:56:14 +0700

    Add files via upload

---
+++ nama.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+        int a;
+        int b;
+        int c;
+
+        cout<<"masukkan nilai a"<<endl;
+        cin>>a;
+        cout<<"masukkan nilai b"<<endl;
+        cin>>b;
+
+        c=a+b;
+        cout<<"hasil penjumlahan dari a dan b adalah: "<<c<<endl;
+    return 0;
+}
