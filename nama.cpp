4689370601a73f442fdda218c4315e18bb3aaea8
Qadri54 <124790452+Qadri54@users.noreply.github.com>
2023-10-04 15:03:30 +0700

    Add files via upload

--- nama.cpp
+++ nama.cpp
@@ -1,16 +1,39 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 int main(){
-        int a;
-        int b;
-        int c;
+    long int gol;
+    char nama[25];
+    int Mkerja;
+    float bonus;
+    long int gajip;
+    double gajib;
 
-        cout<<"masukkan nilai a"<<endl;
-        cin>>a;
-        cout<<"masukkan nilai b"<<endl;
-        cin>>b;
+    cout<<"masukkan nama: ";
+    cin>>nama;
+    cout<<"masukkan golongan: ";
+    cin>>gol;
+    cout<<"masa kerja: ";
+    cin>>Mkerja;
 
-        c=a+b;
-        cout<<"hasil penjumlahan dari a dan b adalah: "<<c<<endl;
+    if(gol == 1 && Mkerja < 5){
+        gol = 5000000;
+        bonus = gol*0.05;
+        gajib = gol + bonus;
+        cout<<"gaji bersih adalah: "<<setprecision(8)<<gajib<<endl;
+    }else if(gol == 2 && Mkerja >= 5){
+        gol = 7000000;
+        bonus = gol * 0.07;
+        gajib = gol + bonus;
+        cout<<"gaji bersih adalah: "<<setprecision(8)<<gajib<<endl;
+    }else if(gol == 3 && Mkerja >= 5){
+        gol = 10000000;
+        bonus = gol * 0.07;
+        gajib = gol + bonus;
+        cout<<"gaji bersih adalah: "<<setprecision(8)<<gajib<<endl;
+    }else{
+        cout<<"salah memsukkan input";
+    }
+        
     return 0;
 }
